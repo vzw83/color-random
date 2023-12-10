86832dbfb4e7728e4f61e258cb49fc31cdbd7faf
vzw83 <vzw83@yandex.ru>
2023-12-10 14:20:31 +0300

    restartColor

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const cols = document.querySelectorAll('.col')
 
+document.addEventListener('keydown', event=>{
+    console.log(event.code)
+})
+
 function generateRandomColor(){
     // RGB
     // #FF0000 red
     // #00FF00 blue
     // #0000FF green
-
     const hexCodes = '0123456789ABCDEF'
-
     let color = ''
     for(let i = 0; i<6; i++){
         color += hexCodes[Math.floor(Math.random()*hexCodes.length)]
@@ -17,7 +19,19 @@ function generateRandomColor(){
 
 function setRandomColors (){
     cols.forEach(col=>{
-        console.log(col)
+        const text = col.querySelector('h2')
+        const button = col.querySelector('button')
+        const color = generateRandomColor() //const color = chroma.random() (chroma библиотека)
+        text.textContent = color
+       col.style.background = color
+       setTextColor(text, color)
+       setTextColor(button, color)
     })
+
 }
+function setTextColor(text, color){
+   const luminance = chroma(color).luminance()
+   text.style.color = luminance > 0.5 ? 'black' : 'white'
+}
+
 setRandomColors()
