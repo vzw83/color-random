c6318067bea2d728bf2ffd21b70a34d836f5b461
vzw83 <vzw83@yandex.ru>
2023-12-10 14:53:34 +0300

    lock-blok

--- script.js
+++ script.js
@@ -1,37 +1,50 @@
-const cols = document.querySelectorAll('.col')
+const cols = document.querySelectorAll(".col");
 
-document.addEventListener('keydown', event=>{
-    console.log(event.code)
-})
+document.addEventListener("keydown", (event) => {
+    event.preventDefault()
+  if (event.code.toLowerCase() === "space") {
+    setRandomColors();
+  }
+});
 
-function generateRandomColor(){
-    // RGB
-    // #FF0000 red
-    // #00FF00 blue
-    // #0000FF green
-    const hexCodes = '0123456789ABCDEF'
-    let color = ''
-    for(let i = 0; i<6; i++){
-        color += hexCodes[Math.floor(Math.random()*hexCodes.length)]
-    }
-    return '#' + color
+document.addEventListener("click", (event) => {
+  const type = event.target.dataset.type;
+  if (type === "lock") {
+    const node = event.target.tagName.toLowerCase()==='i' 
+    ? event.target
+    : event.target.children[0]
+    node.classList.toggle('fa-unlock-alt')
+    node.classList.toggle('fa-lock')
+  }
+});
+
+function generateRandomColor() {
+  // RGB
+  // #FF0000 red
+  // #00FF00 blue
+  // #0000FF green
+  const hexCodes = "0123456789ABCDEF";
+  let color = "";
+  for (let i = 0; i < 6; i++) {
+    color += hexCodes[Math.floor(Math.random() * hexCodes.length)];
+  }
+  return "#" + color;
 }
 
-function setRandomColors (){
-    cols.forEach(col=>{
-        const text = col.querySelector('h2')
-        const button = col.querySelector('button')
-        const color = generateRandomColor() //const color = chroma.random() (chroma библиотека)
-        text.textContent = color
-       col.style.background = color
-       setTextColor(text, color)
-       setTextColor(button, color)
-    })
-
+function setRandomColors() {
+  cols.forEach((col) => {
+    const text = col.querySelector("h2");
+    const button = col.querySelector("button");
+    const color = generateRandomColor(); //const color = chroma.random() (chroma библиотека)
+    text.textContent = color;
+    col.style.background = color;
+    setTextColor(text, color);
+    setTextColor(button, color);
+  });
 }
-function setTextColor(text, color){
-   const luminance = chroma(color).luminance()
-   text.style.color = luminance > 0.5 ? 'black' : 'white'
+function setTextColor(text, color) {
+  const luminance = chroma(color).luminance();
+  text.style.color = luminance > 0.5 ? "black" : "white";
 }
 
-setRandomColors()
+setRandomColors();
